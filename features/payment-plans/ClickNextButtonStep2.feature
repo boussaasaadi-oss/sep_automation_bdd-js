532e9d73504ad7855f866e68c3b113f79e6235fa
boussaasaadi-oss <boussaa.saadi@gmail.com>
2026-04-12 13:47:45 -0400

    sep-16 click button on payment paln page

--- features/payment-plans/ClickNextButtonStep2.feature
+++ features/payment-plans/ClickNextButtonStep2.feature
@@ -2,7 +2,7 @@
 Feature: Click on the next button on payment plans page   #! Test Only
 
     As a customer, I should be able to click on the next button on step 2,
-        when I select a plan.
+    when I select a plan.
 
     #* AC1: Clicking on any plan should activate the next button
     #* AC2: When the customer clicks on the next button, the Step 3 page should be displayed.
@@ -15,53 +15,44 @@ Feature: Click on the next button on payment plans page   #! Test Only
 
     Background:
         Given the user is on the enrollment page
-        And user has completed steps one with valid information
-        And user is on step two of the enrollment process
+        Then user has completed steps one with valid information
+
 
     #TODO: Create scenarios that cover all the acceptance criteria
-@sep16-1
+    @sep16-1
     Scenario: Verify that the next button is disabled by default
-       
-        Then the next button is disabled by default
 
-@sep16-2
-    Scenario: Verify that the next button will be activated when user selects installements payment option
-        
+        Then the next button is disabled by default
         When user clicks on the installements payment option
         Then the next button should be enabled
 
-@sep16-3
-    Scenario: verify that the next button will be activated when user selects installements payment option
-        # Given user is on the enrollment page
-        # And user has completed step one with valid information
-        # And user is on the step two of the enrollment process
-        When user clicks on the installements payment option
-        Then the next button will be enabled
+    @sep16-2
+    Scenario: Verify that the next button will be activated when user selects installements payment option
 
-@sep16-4
-    Scenario: verify that the next button will navigate to step 3 page when the user clicks on it
-        # Given user is on the enrollment page
-        # And user has completed step one with valid information
-        # And user is on the step two of the enrollment process
-        And user clicks on the upfront payment option
-        When user clicks on the next button
+        Then the stepper should display steps 1 in green
+        And  the stepper should display steps 2 in blue
+        When the user selects the installements payment option
+        And  the user clicks on the next button
         Then the step 3 page should be displayed
-@sep16-5
-    Scenario: verify that the stepper should display correct colors for steps 1, 2, and 3
-        # Given user is on the enrollment page
-        # And user has completed step one with valid information
-        # And user is on the step two of the enrollment process
-        And user clicks on the upfront payment option
-        When user clicks on the next button
-        Then the stepper should display steps 1 and 2 in green and step 3 in blue       
-@sep16-6
-    Scenario: Verify that The payment component, price summary, back button, and pay button are displayed on step 3 page
-        # Given user is on the enrollment page
-        # And user has completed step one with valid information
-        # And user is on the step two of the enrollment process
-        And user clicks on the upfront payment option
-        When user clicks on the next button
-        Then the payment component should be displayed
-        And the price summary should be displayed
-        And the back button should be displayed
-        And the pay button should be displayed by default
+        And  the stepper should display steps 1 in green
+        And the stepper should display steps 2 in green
+        And the stepper should display steps 3 in blue
+
+
+
+    @sep16-4
+    Scenario: Verify installments price summary details
+
+        When user clicks on the installements payment option
+        Then the installments price breakdown should be displayed
+
+    @sep16-5
+    Scenario: verify that the back button is displayed and enabled
+
+        Then the back button is enabled
+        When the user clicks the back button
+        Then the step 1 stepper should be blue
+
+
+
+
--- steps/payment-plans/clickNextButtonStep2.js
+++ steps/payment-plans/clickNextButtonStep2.js
@@ -1,39 +1,106 @@
 import { Given, Then, When } from "@cucumber/cucumber";
-import { expect} from "@playwright/test";
-import { paymentPlanPage, page, startApplicationPage } from "../../globalPagesSetup.js";
-import { productInfo } from "../../utilities/qa-data-reader.js";
+import { expect } from "@playwright/test";
+import {
+  paymentPlanPage,
+  reviewPaymentPage,
+  startApplicationPage,
+} from "../../globalPagesSetup.js";
 import { faker } from "@faker-js/faker";
 
-Given('the user is on the enrollment page', async function () {
-    await startApplicationPage.login();
+const STEPPER_COLORS = Object.freeze({
+  green: "rgb(172, 245, 138)",
+  blue: "rgb(1, 201, 255)",
 });
 
-Given('user has completed steps one with valid information', async function () {
-    await startApplicationPage.enterFirstName(faker.person.firstName());
-    await startApplicationPage.enterLastName(faker.person.lastName());
-    await startApplicationPage.enterEmail(faker.internet.email());
-    await startApplicationPage.enterPhoneNumber(faker.phone.number('111 222 3333'));
-    await startApplicationPage.selectHowDidYouHearAboutUs("Google");
-    await startApplicationPage.clickNextButton();
-    //await startApplicationPage.page.waitForTimeout(3000);
-    
+Given("the user is on the enrollment page", async function () {
+  await startApplicationPage.login();
 });
 
-Given('user is on step two of the enrollment process', async function () {
-    await expect(paymentPlanPage.inactiveNextButton).toBeVisible();
-    await paymentPlanPage.page.waitForTimeout(2000);
+Then("user has completed steps one with valid information", async function () {
+  await startApplicationPage.enterFirstName(faker.person.firstName());
+  await startApplicationPage.enterLastName(faker.person.lastName());
+  await startApplicationPage.enterEmail(faker.internet.email());
+  await startApplicationPage.enterPhoneNumber("111 222 3333");
+  await startApplicationPage.selectHowDidYouHearAboutUs("Google");
+  await startApplicationPage.clickNextButton();
+  await expect(paymentPlanPage.chooseAPaymentPlanText).toBeVisible();
 });
 
-Then('the next button is disabled by default', async function () {
-    await expect(paymentPlanPage.inactiveNextButton).toBeDisabled();
-    await paymentPlanPage.page.waitForTimeout(2000);
+Then("the next button is disabled by default", async function () {
+  await expect(paymentPlanPage.nextButton).toBeVisible();
+  await expect(paymentPlanPage.nextButton).toBeDisabled();
 });
 
 When("user clicks on the installements payment option", async function () {
-  await paymentPlanPage.selectPaymentPlan("Installements");
+  await paymentPlanPage.selectPaymentPlan("5 Installments");
 });
 
-Then('the next button should be enabled', async function () {
-    await expect(paymentPlanPage.activeNextButton).toBeEnabled();
-    await paymentPlanPage.page.waitForTimeout(2000);
+When("the user selects the installements payment option", async function () {
+  await paymentPlanPage.selectPaymentPlan("5 Installments");
+});
+
+When("the user clicks on the next button", async function () {
+  await paymentPlanPage.clickNextButton();
+});
+
+Then("the next button should be enabled", async function () {
+  await expect(paymentPlanPage.nextButton).toBeVisible();
+  await expect(paymentPlanPage.nextButton).toBeEnabled();
+});
+
+Then(
+  "the stepper should display steps {int} in green",
+  async function (stepNumber) {
+    await expect(paymentPlanPage.getStepCircle(stepNumber)).toHaveCSS(
+      "background-color",
+      STEPPER_COLORS.green,
+    );
+  },
+);
+
+Then(
+  "the stepper should display steps {int} in blue",
+  async function (stepNumber) {
+    await expect(paymentPlanPage.getStepCircle(stepNumber)).toHaveCSS(
+      "background-color",
+      STEPPER_COLORS.blue,
+    );
+  },
+);
+
+Then("the step {int} page should be displayed", async function (stepNumber) {
+  await expect(paymentPlanPage.getStepCircle(stepNumber)).toHaveCSS(
+    "background-color",
+    STEPPER_COLORS.blue,
+  );
+});
+
+Then("user clicks on the next button", async function () {
+  await paymentPlanPage.clickNextButton();
+});
+
+Then("the payment component should be displayed", async function () {
+  await expect(reviewPaymentPage.paymentForm).toBeVisible();
+});
+
+Then("the installments price breakdown should be displayed", async function () {
+  await expect(paymentPlanPage.basePriceTextUnderInstallments).toBeVisible();
+  await expect(paymentPlanPage.installmentsTextUnderInstallments).toBeVisible();
+  await expect(paymentPlanPage.pricePerInstallmentsTextUnderInstallments,).toBeVisible();
+  await expect( paymentPlanPage.firstMonthPaymentTextUnderInstallments,).toBeVisible();
+});
+
+Then("the back button is enabled", async function () {
+  await expect(paymentPlanPage.backButton).toBeEnabled();
+});
+
+When("the user clicks the back button", async function () {
+  await paymentPlanPage.backButton.click();
+});
+
+Then("the step 1 stepper should be blue", async function () {
+  await expect(paymentPlanPage.getStepCircle(1)).toHaveCSS(
+    "background-color",
+    STEPPER_COLORS.blue,
+  );
 });
